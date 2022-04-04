4fc0f766569d7d8cd7aeaa92464c07602018c549
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-05 03:23:11 +0600

    607 Sales person sql

--- 607-Sales-person.sql
+++ 607-Sales-person.sql
@@ -1,3 +1,3 @@
 SELECT name FROM SalesPerson 
 WHERE sales_id NOT IN (SELECT sales_id FROM Orders WHERE com_id 
-                   IN(SELECT com_id FROM Company WHERE name="RED"))
+                   IN (SELECT com_id FROM Company WHERE name="RED"))
