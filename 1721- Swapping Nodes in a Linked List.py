d44ffc949cc9bea8e69a246306c7199a57e652f0
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-04 19:47:16 +0600

    1721 -Swapping Nodes in a linked list

---
+++ 1721- Swapping Nodes in a Linked List.py
@@ -0,0 +1,17 @@
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        f_node = last_node = head
+        temp = f_node.next
+        
+        for i in range(k-1):
+            f_node = f_node.next # kth value from the first
+            temp = temp.next
+            
+        while temp:
+            temp = temp.next
+            last_node = last_node.next # kth value from the last
+                 
+        last_node.val,  f_node.val = f_node.val, last_node.val #Swap the value
+        
+        
+        return head
