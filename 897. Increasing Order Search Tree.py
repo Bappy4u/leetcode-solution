779c7b77aa12e6bd83660b22e974cf6899f45452
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-14 19:32:04 +0600

    897. Increasing Order Search Tree

---
+++ 897. Increasing Order Search Tree.py
@@ -0,0 +1,24 @@
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        res = []
+        
+        def readBST(nroot):
+            if not nroot:
+                return
+            if nroot.left:
+                readBST(nroot.left)
+            res.append(nroot.val)
+            if root.right:
+                readBST(nroot.right)
+            
+            
+        
+        readBST(root)
+        resNode = TreeNode(0)
+        temp = resNode
+        for i in res:
+            temp.right = TreeNode(i)
+            temp = temp.right
+            
+        return resNode.right
+            
