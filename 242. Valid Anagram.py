627bd0770eef39cdaea551e498d9599adeb72b20
Najmul H. Bappy <http.bappy@gmail.com>
2022-04-16 03:29:10 +0600

    242. Valid Anagram

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        """
+            it can be solved in oneline
+            --> return sorted(s) == sorted(t)
+        """
+        if len(s) != len(t):
+            return False
+        
+        dic1 = {}
+        dic2 = {}
+        
+        for i in range(len(s)):
+            if s[i] in dic1:
+                dic1[s[i]] +=1
+            else:
+                dic1[s[i]] = 1
+                
+            if t[i] in dic2:
+                dic2[t[i]] +=1
+            else:
+                dic2[t[i]] = 1
+                
+                
+        return dic1==dic2
+                
+            
+        
