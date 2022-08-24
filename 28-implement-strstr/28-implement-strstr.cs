4b482078db75c1162bd100482cb9eca8e3a3b0b2
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-24 23:00:38 +0600

    Time: 105 ms (32.69%), Space: 35.3 MB (47.50%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cs
+++ 28-implement-strstr/28-implement-strstr.cs
@@ -2,8 +2,8 @@ public class Solution {
     public int StrStr(string haystack, string needle) {
         
         for (int i=0; i<haystack.Length-needle.Length+1; i++){
-            string substring = haystack.Substring(i, needle.Length);
-            if (substring==needle){
+            
+            if (haystack.Substring(i, needle.Length)==needle){
                 return i;
             }
         }
