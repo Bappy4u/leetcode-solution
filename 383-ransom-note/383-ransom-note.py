2df358c94857ac0814184b64e8d6507d6b7617e0
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-26 03:59:46 +0600

    Time: 144 ms (16.44%), Space: 14.2 MB (20.43%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,26 +1,20 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dic1 = {key: 0 for key in ransomNote}
+        dic2 = {key: 0 for key in magazine}
+ 
+        for key in ransomNote:
+            dic1[key] +=1
+            
+        for key in magazine:
+            dic2[key] +=1
+            
         
-        
-        dic1 = {}
-        dic2 = {}
-        
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in dic1:
-                dic1[ransomNote[i]] +=1
-            else:
-                dic1[ransomNote[i]] = 1
-                
-        for i in range(len(magazine)):        
-            if magazine[i] in dic2:
-                dic2[magazine[i]] +=1
-            else:
-                dic2[magazine[i]] = 1
-                
-                
-        for v in dic1:
-
-            if v not in dic2 or dic1[v]>dic2[v]:
+        for key in dic1:
+            if not dic2.get(key) or dic2[key]<dic1[key]:
                 return False
             
+        
         return True
+        
+        
