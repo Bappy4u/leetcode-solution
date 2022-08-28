5b55a52c277428b2f1425754712d30fff63e2b44
Najmul H. Bappy <http.bappy@gmail.com>
2022-08-29 03:55:55 +0600

    Time: 73 ms (98.50%), Space: 37.3 MB (11.48%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cs
@@ -0,0 +1,52 @@
+public class Solution {
+    public string DecodeString(string s) {
+        Stack<string> characters = new Stack<string>();
+        Stack digits = new Stack();
+        var i = 0;
+        while (i<s.Length){
+            if (Char.IsDigit(s[i])){
+                var d = 0;
+                while(Char.IsDigit(s[i])){
+                    
+                    d = (d*10) + int.Parse(s[i].ToString());
+                    
+                    i++;
+                }
+                
+                digits.Push(d);
+            }
+            
+            else if (s[i].Equals(']')){
+                
+                var temp = "";
+                
+                while (true){
+                    var x = characters.Pop();
+                    
+                    if (x.Equals("[")) break;
+                    
+                    temp = x + temp;
+                }
+                Console.WriteLine(temp);
+                string str = string.Concat(Enumerable.Repeat(temp, (int)digits.Pop()));
+                Console.WriteLine(str);
+                characters.Push(str);
+                i++;
+            }
+            
+            else{
+                characters.Push(s[i].ToString());
+                i++;
+            }
+        }
+        
+        string res = "";
+        
+        foreach(var st in characters){
+            
+            res = st + res;
+        }
+        
+        return res;
+    }
+}
