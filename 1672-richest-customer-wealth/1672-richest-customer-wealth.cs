a3e73ba40b1b7cebae7544becc53a9cef992100b
Najmul H. Bappy <http.bappy@gmail.com>
2022-09-03 12:52:13 +0600

    Time: 147 ms (31.09%), Space: 37.9 MB (22.09%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cs
@@ -2,12 +2,9 @@ public class Solution {
     public int MaximumWealth(int[][] accounts) {
         int res = 0;
         foreach(var ac in accounts){
-            var sum = 0;
-            foreach(var e in ac){
-                sum +=e;
-            }
             
-            res = Math.Max(res, sum);
+            
+            res = Math.Max(res, ac.Sum());
         }
         
         return res;
